c580432bee693c227df0bcbf4ed0511c9ebe7cdf
George  Pianka <gpianka1@gmail.com>
2018-02-21 07:02:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,21 +9,65 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(itemName) {
+  let newObj = {[itemName] : Math.floor (Math.random()*100)};
+  cart.push(newObj);
+  console.log (`${itemName} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+  console.log ("Your shopping cart is empty.");
+  return;
+  }
+  var cartStart = 'In your cart, you have ';
+  for (let i=0; i<cart.length; i++) {
+     let item = cart[i];
+     let itemName = Object.keys(item)[0];
+     let itemPrice = Object.values(item)[0];
+     cartStart +=`${itemName} at $${itemPrice}`;
+     if (i === cart.length - 1){
+      cartStart += '.';
+    } else if (cart.length === 2) {
+      cartStart += ' and ';
+    } else if (i === cart.length - 2) {
+      cartStart += ', and ';
+    } else {
+      cartStart += ', ';
+    }
+    console.log (cartStart);
+  }
 }
 
 function total() {
-  // write your code here
-}
+  let total = 0;
+  for (var i = 0; i < cart.length; i++) {
+      let item = cart[i];
+      total += Object.values(item)[0];
+  }
+   return total;
+ }	 
+
 
 function removeFromCart(item) {
-  // write your code here
-}
+  var matchingItem;
+  var matchingItemIndex;
+  for (var i = 0; i < cart.length; i++) {
+    var itemKey = Object.keys(cart[i])[0];
+    if (itemKey === item) {
+      matchingItem = cart[i];
+      matchingItemIndex = i;
+    }
+  }
+  if (matchingItem === undefined) {
+    console.log("That item is not in your cart.");
+    return;
+  }
+  cart.splice(matchingItemIndex, 1);
+  return cart;
+ 	 }
+
 
 function placeOrder(cardNumber) {
   // write your code here
